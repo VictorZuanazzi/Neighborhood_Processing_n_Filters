bc319875b4370df2a7977fef14c1e9f351924768
Victor <victorzuanazzi@gmail.com>
2019-02-20 18:42:23 +0100

    denoise implemented (except for gaussian)

--- Part-1_ Image Enhancement/denoise.m
+++ Part-1_ Image Enhancement/denoise.m
@@ -1,11 +1,39 @@
 function [ imOut ] = denoise( image, kernel_type, varargin)
+% function used to denoise image using a box, median or gaussian filter.
+% imputs:
+%   image (matrix) 
+%   kernel_type (string): 'box', 'median' or 'gaussian'
+%   varargin: int if 'box', [m, n] if 'median' 
+
 
 switch kernel_type
     case 'box'
-        fprintf('Not implemented\n')
+        if isempty(varargin)
+            box_size = 3;
+        else
+            box_size = varargin{1};
+        end
+        imOut = imboxfilt(image, box_size);
+        
     case 'median'
-        fprintf('Not implemented\n')
+        if isempty(varargin)
+            matrix = [3, 3];
+        elseif length(varargin{1}) == 2
+           matrix = varargin{1};   
+        elseif length(varargin{1}) == 1
+           matrix = [varargin{1}, varargin{1}];
+        else
+            matrix = [3, 3];      
+        end
+        
+        imOut = medfilt2(image, matrix);
+
     case 'gaussian'
         fprintf('Not implemented\n')
+    
+    otherwise
+        %box filter of size 3 is used if the kernel_type is not recognized.
+        imOut = denoise( image, 'box', 3);
+        
 end
 end
