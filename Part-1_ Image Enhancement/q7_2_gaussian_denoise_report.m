e6a6e7812516e1e6fa2e150baf4a06e308f0c7f3
Victor <victorzuanazzi@gmail.com>
2019-02-21 15:26:31 +0100

    minor fixes

--- Part-1_ Image Enhancement/q7_2_gaussian_denoise_report.m
+++ Part-1_ Image Enhancement/q7_2_gaussian_denoise_report.m
@@ -4,10 +4,10 @@
 PRINT_ON_IMAGE = true;
 
 kernel_type = ["box", "median", "gaussian"];
-box_size = [3, 5, 7];
+box_size = [3, 5, 7, 9];
 label = ["original", "gaussian_noise", "salt_n_peper"];
 images = ["./images/image1.jpg", "./images/image1_gaussian.jpg", "./images/image1_saltpepper.jpg"];
-std = [0.1, 1, 10];
+std = [0.8, 1, 1.2];
 
 true_im = imread(images(1));
 
@@ -19,14 +19,14 @@ for k = 1: length(label)
         for j = 1:length(box_size)
             
             %denoises the image using the specified filter and box size
-            filtered_im{j} = denoise(image, 'gaussian', std(1), box_size(j));
+            filtered_im{j} = denoise(image, 'gaussian', std(i), box_size(j));
             
             % calculate psnr 
             im_psnr = myPSNR(true_im, filtered_im{j});
             
             if (PRINT_ON_IMAGE)
                 % write the psnr and box size on the image
-                text = char(num2str(box_size(j)) + "x" + num2str(box_size(j)) + "str =" + num2str(std(i)) + ": psnr = " +  num2str(im_psnr, 5));
+                text = char(num2str(box_size(j)) + "x" + num2str(box_size(j)) + "std =" + num2str(std(i)) + ": psnr = " +  num2str(im_psnr, 5));
                 position = [w/2, 1];
                 im_text = insertText(filtered_im{j}, position, text);
 
@@ -39,7 +39,8 @@ for k = 1: length(label)
         
         %saves images concatenated
         path = "./images/" + label(k) + "_gaussian_" + num2str(std(i)) + "_filter.png";
-        visualize_n_save(path, image, filtered_im{1}, filtered_im{2}, filtered_im{3})
+        figure()
+        visualize_n_save(path, image, filtered_im{1}, filtered_im{2}, filtered_im{3}, filtered_im{4})
     end
 end
 
