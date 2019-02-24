f47aae47f5cfaaae5c1dc09cae53e8244efe2c09
David Speck <david@saduras.de>
2019-02-24 16:24:58 +0100

    Revert "4.4 correct normalization"
    This reverts commit 2c5d10e40eebb232224bfd11fba95ce4859c2849.

--- Part-2_ Gabor Segmentation/gabor_segmentation.m
+++ Part-2_ Gabor Segmentation/gabor_segmentation.m
@@ -215,8 +215,8 @@ features = reshape(features, numRows * numCols, []);
 % \\ Hint: see http://ufldl.stanford.edu/wiki/index.php/Data_Preprocessing
 %          for more information. \\
 
-features = features - mean(features);
-features = features ./ std(features); 
+features = features - mean(features, 2);
+features = features ./ std(features, 0, 2); 
            % \\ TODO: i)  Implement standardization on matrix called features. 
            %          ii) Return the standardized data matrix.
 
